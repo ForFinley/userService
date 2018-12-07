7f8e110c46802238d1fd0596815051dc23cef84f
Lou <ldcaponi@gmail.com>
2018-12-07 16:36:23 -0500

    Fix issue with username being mistaken for email.

--- controllers/userService/registration.js
+++ controllers/userService/registration.js
@@ -1,54 +1,55 @@
-const httpUtil = require('../utils/httpUtil.js');
-const database = require('../utils/mongoUser.js');
-const cryptoUtil = require('../utils/crypto.js');
-const nodemailer = require('../utils/nodemailer.js');
+const httpUtil = require("../utils/httpUtil.js");
+const database = require("../utils/mongoUser.js");
+const cryptoUtil = require("../utils/crypto.js");
+const nodemailer = require("../utils/nodemailer.js");
 
 function validate(body, res) {
-
-    if (!body.username) {
-        res.send(httpUtil.createResponse(400, "ERROR : Missing username."));
-        return false;
-    }
-    if (!body.password) {
-        res.send(httpUtil.createResponse(400, "ERROR : Missing password."));
-        return false;
-    }
-    return true;
+  if (!body.username) {
+    res.send(httpUtil.createResponse(400, "ERROR : Missing username."));
+    return false;
+  }
+  if (!body.password) {
+    res.send(httpUtil.createResponse(400, "ERROR : Missing password."));
+    return false;
+  }
+  return true;
 }
 
 /**
  * Adds user to database
- * @param {*} req 
- * @param {*} res 
+ * @param {*} req
+ * @param {*} res
  */
-module.exports.handler = async function (req, res) {
-    console.log("Starting function registration...");
-    console.log(req.body);
+module.exports.handler = async function(req, res) {
+  console.log("Starting function registration...");
+  console.log(req.body);
 
-    if (req.body === null || !validate(req.body, res)) {
-        return;
-    }
+  if (req.body === null || !validate(req.body, res)) {
+    return;
+  }
 
-    let username = req.body.username;
-    let password = req.body.password;
-    username = username.trim().toLowerCase();
+  let username = req.body.username;
+  let password = req.body.password;
+  let email = req.body.email;
+  username = username.trim().toLowerCase();
 
-    let passwordResult = cryptoUtil.encryptPassword(password);
-    let emailHash = cryptoUtil.emailHashEncrypt(username);
-    let user = await database.queryUserByusername(username);
-    if (user) return res.send(httpUtil.createResponse(400, "ERROR - username in use."));
-    else {
-        let user = {
-            username: username,
-            email: username,
-            password: passwordResult.encryptPass,
-            salt: passwordResult.salt,
-            emailVerified: false,
-            emailVerificationHash: emailHash
-        }
-        console.log("USER: ", user);
-        database.putUser(user);
-        nodemailer.sendEmailVerification(username, emailHash);
-        return res.send(httpUtil.createResponse(200, "SUCCESS : User added."));
-    }
-}
+  let passwordResult = cryptoUtil.encryptPassword(password);
+  let emailHash = cryptoUtil.emailHashEncrypt(username);
+  let user = await database.queryUserByusername(username);
+  if (user)
+    return res.send(httpUtil.createResponse(400, "ERROR - username in use."));
+  else {
+    let user = {
+      username: username,
+      email: email,
+      password: passwordResult.encryptPass,
+      salt: passwordResult.salt,
+      emailVerified: false,
+      emailVerificationHash: emailHash
+    };
+    console.log("USER: ", user);
+    database.putUser(user);
+    nodemailer.sendEmailVerification(email, emailHash);
+    return res.send(httpUtil.createResponse(200, "SUCCESS : User added."));
+  }
+};
--- controllers/utils/nodemailer.js
+++ controllers/utils/nodemailer.js
@@ -1,35 +1,40 @@
-'use strict';
-const nodemailer = require('nodemailer');
+"use strict";
+const nodemailer = require("nodemailer");
 
 function sendEmailVerification(email, emailHash) {
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "ryqanbb@gmail.com",
+      pass: "Ilovebeyblades!"
+    }
+  });
 
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            user: "ryqanbb@gmail.com",
-            pass: "Ilovebeyblades!"
-        }
-    });
+  // let url = "http://localhost:4000/verify/"+emailHash;
+  let url = "http://localhost:3000/verify/" + emailHash;
+  let mailOptions = {
+    from: '"Verify Email" <ryqanbb@gmail.com>',
+    to: email,
+    subject: "Email verification",
+    // text: 'Hello world?',
+    html:
+      "<p>Click link to verify email address.</p> <a href=" +
+      url +
+      ">" +
+      url +
+      "</a>"
+  };
 
-    let url = "http://localhost:4000/verify/"+emailHash;
-    let mailOptions = {
-        from: '"Verify Email" <ryqanbb@gmail.com>',
-        to: email,
-        subject: 'Email verification',
-        // text: 'Hello world?',
-        html: '<p>Click link to verify email address.</p> <a href='+url+'>'+url+'</a>'
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-    });
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message sent: %s", info.messageId);
+  });
 }
 
 module.exports = {
-    sendEmailVerification
-}
+  sendEmailVerification
+};
